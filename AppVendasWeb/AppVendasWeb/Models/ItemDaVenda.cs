fed7fc59399b653bfbbaaa56bee81fdc16a5b9a3
Karol Santos <karolaynesouzasantos80@gmail.com>
2024-02-08 17:06:05 -0300

    funcionalidades feitas

---
+++ AppVendasWeb/AppVendasWeb/Models/ItemDaVenda.cs
@@ -0,0 +1,16 @@
+﻿namespace AppVendasWeb.Models
+{
+    public class ItemDaVenda
+    {
+        public Guid ItemDaVendaId { get; set; }
+        public Guid ProdutoId { get; set; }
+
+        public Produto? Produto { get; set; }
+
+        public Guid VendaID { get; set; }
+
+        public Venda? Venda { get; set; }
+        public int Quantidade { get; set; }
+        public decimal Valor { get; set; }
+    }
+}
